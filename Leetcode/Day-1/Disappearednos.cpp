378396b60ff487abc244aca6e40f6735ad82b399
Charu Dwivedi <82705387+Charud25@users.noreply.github.com>
2021-11-02 16:45:15 +0530

    Update Disappearednos.cpp

--- Leetcode/Day-1/Disappearednos.cpp
+++ Leetcode/Day-1/Disappearednos.cpp
@@ -1,1 +1,32 @@
-
+class Solution {
+public:
+        
+void swap(vector<int>& a,int first, int second)
+{
+    int temp = a[first];
+    a[first] = a[second];
+    a[second] = temp;
+}
+vector<int> findDisappearedNumbers(vector<int>& a) {
+    int i=0;
+    vector<int> nums={};
+    while(i < a.size())
+{
+        int correct = a[i]-1;
+       if( a[i] != a[correct] ) 
+       {
+           swap(a,i,correct);
+       }
+       else
+            i++;
+}
+        for(int i=0;i<a.size();i++)
+        {
+            if(a[i]!=i+1)
+            {
+                nums.push_back(i+1);
+            }
+        }
+  return nums;
+    }
+};
