ed51e902cb7ca2b7ed7ce1dd1d8bd32e3a483236
Charu Dwivedi <82705387+Charud25@users.noreply.github.com>
2022-04-12 18:44:19 +0530

    Time: 4 ms (76.21%), Space: 8.1 MB (45.34%) - LeetHub

--- 1252-cells-with-odd-values-in-a-matrix/1252-cells-with-odd-values-in-a-matrix.cpp
+++ 1252-cells-with-odd-values-in-a-matrix/1252-cells-with-odd-values-in-a-matrix.cpp
@@ -3,20 +3,26 @@ public:
     int oddCells(int m, int n, vector<vector<int>>& indices) {
             
         int res=0;
+        //2-D array with all zeroes 
         vector<vector<int>> v(m,vector<int>(n,0));
         for(int i=0;i<indices.size();i++)
         {
             int r=indices[i][0];
             int c=indices[i][1];
+            
+            //Increment all the cells on row r
             for(int j=0;j<n;j++)
             {
                 v[r][j]++;
             }
+            
+            //Increment all the cells on column c
             for(int j=0;j<m;j++)
             {
                 v[j][c]++;
             }
         }
+        
         for(int i=0;i<m;i++)
         {
             for(int j=0;j<n;j++)
