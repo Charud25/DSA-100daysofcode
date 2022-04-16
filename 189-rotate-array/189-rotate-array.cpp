18faf83046d9c5c2096818fd9f5b36c3fb05edd7
Charu Dwivedi <82705387+Charud25@users.noreply.github.com>
2022-04-16 22:43:27 +0530

    Time: 44 ms (40.52%), Space: 25 MB (52.13%) - LeetHub

--- 189-rotate-array/189-rotate-array.cpp
+++ 189-rotate-array/189-rotate-array.cpp
@@ -1,22 +1,18 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
+     
+       //remove redundant k values  and shorten k
+        k = k % nums.size();
         
-    vector<int>v;
+		//reverse the array from starting to end
+        reverse(nums.begin(), nums.end());
+		
+		//reverse from starting till kth element
+        reverse(nums.begin(), nums.begin()+k);
+		
+		//reverse from kth till last element
+        reverse(nums.begin()+k, nums.end());
         
-    if(k>nums.size())
-        k=k%nums.size();
-        
-    int n=nums.size()-k;
-    for(int i=n;i<nums.size();i++)
-    {
-        v.push_back(nums[i]);
-    }
-    for(int i=0;i<n;i++)
-    {
-        v.push_back(nums[i]);
-    }
-    nums.clear();
-    nums=v;
     }
 };
