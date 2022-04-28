c487a2909db64b441a6620808dd01606df9f3a3b
Charu Dwivedi <82705387+Charud25@users.noreply.github.com>
2022-04-28 14:25:56 +0530

    Time: 8 ms (71.28%), Space: 11.8 MB (56.99%) - LeetHub

--- 61-rotate-list/61-rotate-list.cpp
+++ 61-rotate-list/61-rotate-list.cpp
@@ -10,21 +10,21 @@
  */
 class Solution {
 public:
-
-      ListNode* rotateRight(ListNode* head, int k) {
-        ListNode *curr=head,*p;
-        int len=0;
-        if(!head || !head->next || k==0)    return head;     // edge cases
-       
-          while(curr){     // calculate length of  linked list
-            len++;
-            curr=curr->next;
-           }
+    ListNode* rotateRight(ListNode* head, int k) {
         
-          curr=head;
-          k=k%len;  //when k>n //calculating the number of actual rotations                             
-        while(k){   
-            // rotating the list by 'k' times
+       if(!head || !head->next || k==0)    return head;     // edge cases
+       ListNode* p ; 
+        ListNode* curr = head ;
+        int len = 0;
+        while(curr)
+        {
+            len++;
+            curr= curr->next;
+        }
+        
+        k=k%len;
+        curr = head;
+        while(k){     
             while(curr->next){
                 p=curr;
                 curr=curr->next;
@@ -35,6 +35,6 @@ public:
             k--;
         }
         return head;
-      
+        
     }
 };
